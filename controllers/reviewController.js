593c3d2bbb37db950d40d1f48b258d251ee7494f
Siddharth Mishra <siddharthmishra2709@gmail.com>
2025-07-01 18:54:23 +0530

    Added Feature for Average Rating

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,11 +1,13 @@
+const Book = require('../models/Book');
 const Review = require('../models/Review');
 
-// Add a review to a book
+// Add review
 exports.addReview = async (req, res) => {
   const { rating, comment } = req.body;
   const bookId = req.params.bookId;
 
   try {
+    // Save the review
     const review = await Review.create({
       rating,
       comment,
@@ -13,11 +15,18 @@ exports.addReview = async (req, res) => {
       book: bookId
     });
 
+    // Recalculate average rating
+    const reviews = await Review.find({ book: bookId });
+    const average = reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length;
+
+    await Book.findByIdAndUpdate(bookId, { averageRating: average.toFixed(1) });
+
     res.status(201).json(review);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 };
+
 
 // Get all reviews of the logged-in user
 exports.getMyReviews = async (req, res) => {
--- models/Book.js
+++ models/Book.js
@@ -4,12 +4,14 @@ const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: String,
   description: String,
+  averageRating: { type: Number, default: 0 }, // ✅ New field
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, {
   timestamps: true,
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
+
 
 // ✅ Add virtual populate for reviews
 bookSchema.virtual('reviews', {
